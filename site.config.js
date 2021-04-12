3530deb80e90fa63bf0b9f6c74d8fbf6eccd4be0
OneForNothing <31834116+OneForNothing@users.noreply.github.com>
2021-04-12 10:34:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Personal-Home-c7e222a7e7e94e9a9af6bfa141b5996c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '可可爱爱平泽唯',
+  domain: 'https://www.notion.so/Personal-Home-c7e222a7e7e94e9a9af6bfa141b5996c',
+  author: 'Jie Jin',
 
   // open graph metadata (optional)
   description: 'Example site description',
